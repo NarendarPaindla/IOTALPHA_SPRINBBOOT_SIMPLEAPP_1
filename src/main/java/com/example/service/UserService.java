1c92fac5e06f463a2a942c388ca75f50ee256024
narendarpaindla <narendarreddypaindla@gmail.com>
2025-03-06 06:41:00 +0000

    s2

--- src/main/java/com/example/service/UserService.java
+++ src/main/java/com/example/service/UserService.java
@@ -1,5 +1,7 @@
 package com.example.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,5 +18,8 @@ public class UserService {
     public User createUser(User user){
         return userRepository.save(user);
     }
+        public List<User> getAllUsers(){
+            return userRepository.findAll();
+        }
 
 }
