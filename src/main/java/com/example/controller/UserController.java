f0a7991bcb27ba39126cf724c0e7a66535ff6916
narendarpaindla <narendarreddypaindla@gmail.com>
2025-03-06 07:19:52 +0000

    c3

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -1,10 +1,15 @@
 package com.example.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,5 +31,22 @@ public class UserController {
     @GetMapping("/listofuser")
     public List<User> getUsers(){
         return userService.getAllUsers();
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable String id,@RequestBody  User userDetails){
+        Optional<User> existingUser = userService.getUserByEmail(userDetails.getEmail());
+        if (existingUser.isPresent()) {
+            User user = existingUser.get();
+            user.setName(userDetails.getName());
+            user.setEmail(userDetails.getEmail());
+            User updatedUser = userService.updateUser(user);
+            return ResponseEntity.ok(updatedUser);
+        }
+        return ResponseEntity.notFound().build();
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable String id) {
+        userService.deleteUser(id);
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/example/service/UserService.java
+++ src/main/java/com/example/service/UserService.java
@@ -1,6 +1,7 @@
 package com.example.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,5 +22,13 @@ public class UserService {
         public List<User> getAllUsers(){
             return userRepository.findAll();
         }
-
+        public User updateUser(User user) {
+            return userRepository.save(user);
+        }
+        public Optional<User> getUserByEmail(String email) {
+            return userRepository.findByEmail(email);
+        }
+        public void deleteUser(String id) {
+            userRepository.deleteById(id);
+        }
 }
